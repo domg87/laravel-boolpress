1099c80bd4d64ad0e7583ac991502ed76235d3cf
dom <domepiro87@yahoo.it>
2020-11-09 19:22:45 +0100

    creazione_index_pubblica

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,1 +1,30 @@
-@dd($articles)
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Contenuto</th>
+                    <th scope="col">Azioni</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($articles as $article)
+                    <tr>
+                        <td>{{$article->title}}</td>
+                        <td>{{$article->slug}}</td>
+                        <td>{{$article->content}}</td>
+                        <td>
+                            <a href="{{route('admin.posts.show', $article->slug)}}">View</a>
+                            <a href="#">Edit</a>
+                            <a href="#">Delite</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
